ad25c100bb55db9b9290de9707d77889f30cdb16
Michael Guerrero <michaelguerrero@uagateway.org>
2018-12-10 23:14:38 +0000

    adding

--- js/script.js
+++ js/script.js
@@ -1,14 +1,81 @@
 /* global $ */
 
-$(document).ready(function() {
-    $("button").click(function() {
-        var name = $("#name").val();
+
+  
+
+
+
+ $("button").click(function() {
+
+      var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
-    });
+
+
+
+  
+
+
+function q1 (){
+    
+    
+if (q1Result == "Often"){
+    
+totalScore + 15;    
+    
+}
+
+else if (q1Result == "not often"){
+    
+totalScore + 5;    
+    
+}
+
+else{
+
+totalScore + 1;    
+    
+}    
+    
+}
+
+function q2 (){
+    
+    
+if (q2Result == "Yes"){
+    
+totalScore + 15;    
+    
+}
+
+else {
+    
+totalScore + 5;    
+    
+}
+  
+}
+
+function q3 (){
+    
+    
+if (q3Result == "Work"){
+    
+totalScore + 20;    
+    
+}
+
+else{
+    
+totalScore + 1;    
+    
+}
+
+    
+}
 
 });
